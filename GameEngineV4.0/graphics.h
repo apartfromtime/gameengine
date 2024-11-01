e395b28c51befc3e92ec608050dc8fe924b137c7
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-11-01 12:18:15 +1100

    Dynamically link utility library (#10)

--- GameEngineV4.0/graphics.h
+++ GameEngineV4.0/graphics.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <SDL3\SDL.h>
-#include <SDL3\SDL_image.h>
-#include <GEUL\g_math.h>
+#include <GEUL\g_geul.h>
 #include "constants.h"
 #include "gameError.h"
 #include "sprite.h"
